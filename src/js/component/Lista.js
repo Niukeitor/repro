ce200263c70d0e3aa8a7834bf06233ed84991f30
Maikol Moreira <Niukeitor@hotmail.com>
2021-05-03 20:03:51 +0000

    repro

---
+++ src/js/component/Lista.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export function Lista() {
+	return (
+		<div className="list-group">
+			<a href="#" className="list-group-item list-group-item-action">
+				A
+			</a>
+
+			<a
+				href="#"
+				className="list-group-item list-group-item-action list-group-item-primary">
+				A
+			</a>
+			<a
+				href="#"
+				className="list-group-item list-group-item-action list-group-item-secondary">
+				A
+			</a>
+		</div>
+	);
+}
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,31 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { Lista } from "./Lista";
 
 //create your first component
 export function Home() {
+	[
+		{
+			id: 1,
+			category: "game",
+			name: "Mario Castle",
+			url: "files/mario/songs/castle.mp3"
+		},
+		{
+			id: 2,
+			category: "game",
+			name: "Mario Star",
+			url: "files/mario/songs/hurry-starman.mp3"
+		},
+		{
+			id: 3,
+			category: "game",
+			name: "Mario Overworld",
+			url: "files/mario/songs/overworld.mp3"
+		}
+	];
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex justify-content-center align-items-center vh-100">
+			<Lista></Lista>
 		</div>
 	);
 }
